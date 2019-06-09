8894ffe7fb56722dc80c3887390dbabdff9e59ce
Jivant Khatri (JIVO) <44354124+jivantkc@users.noreply.github.com>
2019-06-09 11:21:21 +0800

    updates

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-import App from './components/App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'react-grid-system';
+import LandingPage from './components/villages/Landing Page.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <LandingPage/>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
